4cda451b0edc731d6a3d416c6a88d5088462f1a8
silverhand7 <refojunior5@gmail.com>
2023-09-20 20:54:52 +0800

    update transaction

--- services/transaction_service.go
+++ services/transaction_service.go
@@ -3,6 +3,7 @@ package services
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"time"
 
 	"github.com/go-playground/validator"
@@ -132,10 +133,24 @@ func (service *TransactionService) Update(ctx context.Context, request requests.
 	helpers.PanicIfError(err)
 	defer helpers.CommitOrRollback(tx)
 
-	_, err = service.TransactionRepository.FindById(ctx, tx, request.ID, request.UserID)
+	category, err := service.CategoryRepository.FindById(ctx, tx, request.CategoryID)
 	if err != nil {
 		panic(exceptions.NewNotFoundError(err.Error()))
 	}
+
+	wallet, err := service.WalletRepository.FindById(ctx, tx, request.WalletID, request.UserID)
+	if err != nil {
+		panic(exceptions.NewNotFoundError(err.Error()))
+	}
+
+	t, err := service.TransactionRepository.FindById(ctx, tx, request.ID, request.UserID)
+	if err != nil {
+		panic(exceptions.NewNotFoundError(err.Error()))
+	}
+
+	wallet.Balance += t.Nominal
+
+	fmt.Println(wallet.Balance)
 
 	dateLayout := "2006-01-02 15:04:05"
 	date, err := time.Parse(dateLayout, request.DateTime)
@@ -152,6 +167,18 @@ func (service *TransactionService) Update(ctx context.Context, request requests.
 	}
 
 	transaction = service.TransactionRepository.Update(ctx, tx, transaction)
+
+	if category.Type == "E" {
+		wallet.Balance -= transaction.Nominal
+	} else {
+		wallet.Balance += transaction.Nominal
+	}
+
+	service.WalletRepository.UpdateBalance(ctx, tx, domain.Wallet{
+		ID:        wallet.ID,
+		Balance:   wallet.Balance,
+		UpdatedAt: time.Now().UTC(),
+	})
 
 	return responses.TransactionResponse{
 		ID:         transaction.ID,
