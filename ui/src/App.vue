d9152697567af045b2ea936021c10e3f1f73f534
silverhand7 <refojunior5@gmail.com>
2023-09-30 11:32:56 +0800

    add transaction icon

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -17,6 +17,11 @@ import { RouterLink, RouterView } from 'vue-router'
       <RouterLink to="/wallets" class="py-3 max-w-[25px]">
         <img src="@/assets/images/wallet.png" alt="wallet">
       </RouterLink>
+      <RouterLink to="/transactions/create" class="py-3 max-w-[25px]">
+        <div class="bg-blue-500 text-3xl block pl-4 pr-4 text-center relative w-9 h-10 rounded-full border-4 border-slate-100">
+          <span class="absolute left-[7.5px] -top-1 text-white">+</span>
+        </div>
+      </RouterLink>
       <RouterLink to="/" class="py-3 max-w-[25px]">
         <img src="@/assets/images/trend.png" alt="trend">
       </RouterLink>
